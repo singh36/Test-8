0e7c5c1bafe055450e5ae5006c53335efe93fff3
unknown <anshoosingh36@gmail.com>
2022-06-19 19:47:36 +0530

    first commit

---
+++ question2.js
@@ -0,0 +1,10 @@
+let friends = [{ fname: 'Rachal', lname: "Green", id: 1, marks: 98 }, 
+{ fname: 'Ross', lname: "Geller", id: 2, marks: 23 }, 
+{ fname: 'Monica ', lname: "Geller", id: 3, marks: 75 },
+ { fname: 'Joey', lname: " Tribbiani", id: 4, marks: 46 }, 
+ { fname: 'Chandler  ', lname: "Bing", id: 5, marks: 78 }, 
+ { fname: 'Phoebe', lname: "Buffay", id: 6, marks: 45 }]
+ friends.filter(function(e,i)
+{
+  console.log(e.fname+' '+ e.lname);
+});
